752b333b55b7e8248a0d8099398043f14c8a58d7
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2020-03-12 16:40:52 +0100

    Fontend

--- server/controllers/movie-ctrl.js
+++ server/controllers/movie-ctrl.js
@@ -93,11 +93,6 @@ getMovieById = async (req, res) => {
             return res.status(400).json({ success: false, error: err })
         }
 
-        if (!movie) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Movie not found` })
-        }
         return res.status(200).json({ success: true, data: movie })
     }).catch(err => console.log(err))
 }
