e5462a5127456082c823ca2e34a8452115a29280
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2020-03-12 17:16:35 +0100

    mongodb

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('MongoDB://localhost:27017/cinema', { useNewUrlParser: true })
+    .connect('mongodb://localhost:27017/cinema', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
